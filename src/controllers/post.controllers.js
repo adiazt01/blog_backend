ca7b75e849bee7e64b8ce10f7224a4bdeea38248
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-01-03 19:33:28 -0400

    add route getCountPosts

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,59 +1,65 @@
 import { prisma } from "../app.js";
 
 export const searchPosts = async (req, res) => {
-	const { search } = req.params;
+  const { search } = req.params;
 
-	if (!search) {
-		const posts = await prisma.post.findMany();
-		if (!posts) {
-			res.status(200).json({ message: [] });
-		} else {
-			res.status(200).json(posts);
-		}
-	}
+  if (!search) {
+    const posts = await prisma.post.findMany();
+    if (!posts) {
+      res.status(200).json({ message: [] });
+    } else {
+      res.status(200).json(posts);
+    }
+  }
 
-	const posts = await prisma.post.findMany({
-		where: {
-			OR: [
-				{ title: { contains: search, mode: "insensitive" } },
-				{ content: { contains: search, mode: "insensitive" } },
-			],
-		},
-	});
+  const posts = await prisma.post.findMany({
+    where: {
+      OR: [
+        { title: { contains: search, mode: "insensitive" } },
+        { content: { contains: search, mode: "insensitive" } },
+      ],
+    },
+  });
 
-	res.status(200).json(posts);
+  res.status(200).json(posts);
+};
+
+export const getCountPosts = async (req, res) => {
+  const count = await prisma.post.count();
+  console.log(count);
+  res.status(200).json(count);
 };
 
 export const getPostById = async (req, res) => {
-	const post = await prisma.post.findUnique({
-		where: { id: req.params.id },
-	});
-	if (post === undefined) {
-		return res.status(404).json({ message: "Post not found" });
-	}
-	res.status(200).json({ message: "Get post successfully", post });
+  const post = await prisma.post.findUnique({
+    where: { id: req.params.id },
+  });
+  if (post === undefined) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.status(200).json({ message: "Get post successfully", post });
 };
 
 export const getAllPosts = async (req, res) => {
-	const page = parseInt(req.query.page) || 1;
-	const limit = parseInt(req.query.limit) || 10;
-	const startIndex = (page - 1) * limit;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page - 1) * limit;
 
-	const posts = await prisma.post.findMany({
-		skip: startIndex,
-		take: limit,
-	});
+  const posts = await prisma.post.findMany({
+    skip: startIndex,
+    take: limit,
+  });
 
-	res.status(200).json(posts);
+  res.status(200).json(posts);
 };
 
 export const getLastsFivePost = async (req, res) => {
-	const posts = await prisma.post.findMany({
-		take: 5,
-		orderBy: {
-			createdAt: "desc",
-		},
-	});
+  const posts = await prisma.post.findMany({
+    take: 5,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-	res.status(200).json(posts);
+  res.status(200).json(posts);
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import {
-	getAllPosts,
-	getLastsFivePost,
-	searchPosts,
-	getPostById,
+  getAllPosts,
+  getLastsFivePost,
+  searchPosts,
+  getPostById,
+  getCountPosts,
 } from "../controllers/post.controllers.js";
 
 export const postRoutes = Router();
 
-postRoutes.get("/search/:search", searchPosts);
 postRoutes.get("/", getAllPosts);
-postRoutes.get("/:id", getPostById);
+postRoutes.get("/search/:search", searchPosts);
+postRoutes.get("/count", getCountPosts);
 postRoutes.get("/lasts", getLastsFivePost);
+postRoutes.get("/:id", getPostById);
